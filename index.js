fbb71c6eb03da1d5a847f03f7136b233812c1b63
Alex Moreira <alexbmoreira@gmail.com>
2020-05-16 09:40:36 -0400

    Clicked seats become selected

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const ticket_price = +movie_select.value;
 
 container.addEventListener("click", function (e)
 {
-    if (e.target.classList.contains("seat"))
+    if (e.target.classList.contains("seat") && !e.target.classList.contains("occupied"))
     {
-        console.log(e.target);
+        e.target.classList.toggle("selected")
     }
 });
